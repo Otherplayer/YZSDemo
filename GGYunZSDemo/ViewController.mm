a65bdc44250c7816d44d52fb23b5100f8222b7d2
lel.guo <lel.guo@haihangyun.com>
2016-06-24 16:33:28 +0800

    first commit

---
+++ GGYunZSDemo/ViewController.mm
@@ -0,0 +1,120 @@
+//
+//  ViewController.m
+//  GGYunZSDemo
+//
+//  Created by __无邪_ on 16/6/23.
+//  Copyright © 2016年 __无邪_. All rights reserved.
+//
+#import "Configure.h"
+#import "ViewController.h"
+#import "USCSpeechUnderstander.h"
+#import "USCSpeechResult.h"
+
+
+@interface ViewController ()<USCSpeechUnderstanderDelegate>
+
+@property (nonatomic,strong) USCSpeechUnderstander *speechUnderstander;
+@property (nonatomic,strong) NSMutableString  *asrString;
+@property (weak, nonatomic) IBOutlet UIButton *btnStart;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.asrString = [[NSMutableString alloc]init];
+    // 创建对象
+    USCSpeechUnderstander *speechUnderstander = [[USCSpeechUnderstander alloc]initWithContext:nil appKey:APPKEY secret:SECRET];
+    self.speechUnderstander = speechUnderstander;
+    self.speechUnderstander.delegate = self;
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)startAction:(id)sender {
+    [self.speechUnderstander start];
+    
+}
+#pragma mark -  SpeechUnderstanderDelegate
+
+- (void)onRecognizerResult:(NSString *)result isLast:(BOOL)isLast
+{
+    NSLog(@"一次返回结果");
+    if (result)
+        [self.asrString appendString:result];
+    
+    if(isLast)
+    {
+        NSLog(@"最后一次返回结果");
+    }
+}
+
+- (void)onUnderstanderResult:(USCSpeechResult *)result
+{
+    NSLog(@"返回的语义理解结果 = %@",result.stringResult);
+}
+
+- (void)onSpeechStart
+{
+    NSLog(@"状态:开始说话");
+}
+
+- (void)onRecognizerStart
+{
+    NSLog(@"识别开始");
+}
+
+- (void)onRecordingStop:(NSData *)data
+{
+}
+
+- (void)onRecordingStart
+{
+    NSLog(@"录音开始");
+}
+
+- (void)onVADTimeout
+{
+    NSLog(@"vad timeout");
+}
+
+- (void)onUpdateVolume:(int)volume
+{
+//    self.statusLabel.text = @"状态:正在录音...";
+//    float volumeRate = volume / 100.0f;
+//    
+//    [self.progressView setProgress:volumeRate animated:YES];
+}
+
+- (void)onEnd:(NSError *)error
+{
+    if(error)
+    {
+        NSLog(@"出错了");
+//        self.statusLabel.textColor = [UIColor redColor];
+//        self.statusLabel.text = [NSString stringWithFormat:@"状态:%@",error.domain];
+//        [self.btn setSelected:NO];
+        return;
+    }
+    else if (self.asrString.length == 0 )
+    {
+        NSLog(@"没有听到声音");
+//        self.statusLabel.textColor = [UIColor redColor];
+//        self.statusLabel.text = @"状态:没有听到声音";
+    }
+    NSLog(@"done");
+//    [self.btn setSelected:NO];
+//    self.statusLabel.textColor = [UIColor blackColor];
+//    self.statusLabel.text = @"状态: 语义理解完成";
+}
+
+@end
---
+++ GGYunZSDemo/lib/Configure.h
@@ -0,0 +1,16 @@
+//
+//  Configure.h
+//  USCMusicDemo
+//
+//  Created by yunzhisheng-zy on 14-12-25.
+//  Copyright (c) 2014? usc. All rights reserved.
+//
+
+#ifndef USCMusicDemo_Configure_h
+#define USCMusicDemo_Configure_h
+
+#define APPKEY @"clst4jjqqmustnvncm4x55ouy77bu6fwacxcwzi2"
+
+#define SECRET @"93b380162c5d08ffe8ab2b8ea10fe8e2"
+
+#endif
